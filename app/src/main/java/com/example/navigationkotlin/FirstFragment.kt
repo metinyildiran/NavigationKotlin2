e35bb756dabfe290b7bbf5c12d51a7c6052019cb
Metin Yıldıran <metin.1464@gmail.com>
2021-02-10 19:42:42 +0300

    Bitti

---
+++ app/src/main/java/com/example/navigationkotlin/FirstFragment.kt
@@ -0,0 +1,38 @@
+package com.example.navigationkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.navigationkotlin.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private lateinit var binding: FragmentFirstBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFirstBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button1.setOnClickListener {
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment("Qubits")
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/navigationkotlin/SecondFragment.kt
@@ -0,0 +1,42 @@
+package com.example.navigationkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.navigationkotlin.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSecondBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            binding.textView2.text = SecondFragmentArgs.fromBundle(it).username
+        }
+
+        binding.button2.setOnClickListener {
+            val action = SecondFragmentDirections.actionSecondFragmentToFirstFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
